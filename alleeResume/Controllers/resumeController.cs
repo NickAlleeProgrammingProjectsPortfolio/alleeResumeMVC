899300b0263999168a58d360399e637dd5481618
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-03-04 19:35:39 -0600

    Add project files.

---
+++ alleeResume/Controllers/resumeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alleeResume.Controllers
+{
+    public class resumeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "education page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult workHistory()
+        {
+            ViewBag.Message = "work history page.";
+
+            return View();
+        }
+
+        public ActionResult skills()
+        {
+            ViewBag.Message = "skills page.";
+
+            return View();
+        }
+
+        public ActionResult references()
+        {
+            ViewBag.Message = "references page.";
+
+            return View();
+        }
+
+        public ActionResult projects()
+        {
+            ViewBag.Message = "projects page.";
+
+            return View();
+        }
+    }
+}
